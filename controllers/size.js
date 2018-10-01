29f6797398cedf12f1ed4fd6a8e324070ec5e6e8
breogangf <breogangf@gmail.com>
2018-10-01 14:37:42 +0200

    Testing basic search algorithm using maps

--- controllers/size.js
+++ controllers/size.js
@@ -1,5 +1,6 @@
 const logger = require('../helpers/logger');
 const ProductSize = require('../models/productSize');
+const StockEntry = require('../models/stockEntry');
 
 exports.getSizes = (req, res) => {
     ProductSize.find({}, (errorRetrievingProductSizes, productSizes) => {
@@ -10,6 +11,42 @@ exports.getSizes = (req, res) => {
         if (productSizes.length === 0) {
             return res.status(404).send({ message: 'No productSizes were found' });
         }
-        return res.status(200).send(productSizes);
+
+        StockEntry.find({}, (errorRetrievingStockEntries, stockEntries) => {
+            if (errorRetrievingStockEntries) {
+                logger.log(`Error retrieving stock entries: ${errorRetrievingStockEntries}`);
+                return res.status(500).send({ message: `Error retrieving stock entries: ${errorRetrievingStockEntries}` });
+            }
+            if (stockEntries.length === 0) {
+                return res.status(404).send({ message: 'No stock entries were found' });
+            }
+
+            const stockMap = new Map();
+
+            stockEntries.forEach((stockEntry) => {
+                if (stockEntry.qty > 0) stockMap.set(stockEntry.sizeId, { qty: stockEntry.qty });
+            });
+
+            productSizes.forEach((productSize) => {
+                const stockObject = (stockMap.get(productSize.id));
+                if (stockObject) {
+                    stockMap.set(productSize.id, {
+                        sizeSystem: productSize.sizeSystem,
+                        description: productSize.description,
+                        qty: stockObject.qty
+                    });
+                }
+            });
+
+
+            //Return final Ids
+            const ids = [];
+
+            stockMap.forEach((stockItem, key) => {
+                ids.push(key);
+            });
+
+            return res.status(200).send(ids);
+        });
     });
 };
